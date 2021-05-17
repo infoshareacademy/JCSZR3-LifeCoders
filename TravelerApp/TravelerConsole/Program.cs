39b61c56a7fa334e671e61be3b1e8bae214a4d51
DanielPrzewozny <daniel.przewozny@yahoo.com>
2021-05-17 23:44:30 +0200

    Program.cs: less code. Relative paths

--- TravelerApp/TravelerConsole/Program.cs
+++ TravelerApp/TravelerConsole/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using TravelerAppCore.Models.Hotels;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace TravelerFacebookConsole
 {
@@ -6,7 +11,34 @@ namespace TravelerFacebookConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DeserializeJson();
+        }
+
+        static void DeserializeJson()
+        {
+            string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string sFile = Path.Combine(sCurrentDirectory, @"..\..\..\..\TravelerAppCore\Data\JSON_Hotels");
+            string sFilePath = Path.GetFullPath(sFile);
+            string[] jsonFiles = Directory.GetFiles(sFilePath, "*.json").Select(Path.GetFileName).ToArray();
+            List<Root> targetData = new List<Root>();
+
+            foreach (var item in jsonFiles)
+            {
+                string sFileJson = Path.Combine(sFilePath, item);
+                try
+                {
+                    string jsonFromFiles;
+                    using (StreamReader reader = new StreamReader(sFileJson))
+                    {
+                        jsonFromFiles = reader.ReadToEnd();
+                        targetData.Add(JsonConvert.DeserializeObject<Root>(jsonFromFiles));
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            Console.WriteLine("[POINTER] Koniec deserializacji");
         }
     }
 }
