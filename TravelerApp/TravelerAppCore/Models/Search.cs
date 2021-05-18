a93ce445666f85c13e8c646d496eec08e50498eb
Łukasz Balcerak <ubalcerak@gmail.com>
2021-05-18 14:45:11 +0200

    Added getAdress method, some changes in byLocalistion method

--- TravelerApp/TravelerAppCore/Models/Search.cs
+++ TravelerApp/TravelerAppCore/Models/Search.cs
@@ -7,21 +7,35 @@ using TravelerAppCore.Models.Hotels;
 
 namespace TravelerAppCore.Models
 {
-    public static class Searching
+    public static class Search
     { 
-        public static List<Root> byLocalisation(List<Root> targetData, string localisation)
+        public static List<Root> byLocalisation(List<Root> targetData)
         {
             List<Root> hotelLocalisation = new List<Root>();
+            string adress = getAdress();
 
             foreach(Root hotel in targetData)
             {
-                if(hotel.HotelInfo.Address == localisation)
+                if(hotel.HotelInfo.Address == adress)
                 {
                     hotelLocalisation.Add(hotel);
                 }
             }
 
             return hotelLocalisation;
+        }
+
+        private static string getAdress() {
+            Console.WriteLine("Podaj adress do wyszukiwania:");
+            string adress = Console.ReadLine();
+            if(adress.Length < 4)
+            {
+                Console.WriteLine("Wyszukiwana fraza powinna mieć conajmniej 3 znaki");
+                return getAdress();
+            } else
+            {
+                return adress;
+            }
         }
     }
 }
--- TravelerApp/TravelerConsole/Program.cs
+++ TravelerApp/TravelerConsole/Program.cs
@@ -27,11 +27,11 @@ namespace TravelerFacebookConsole
             rooteList.Add(rootTwo);
             rooteList.Add(rootThree);
 
-            List<Root> rooteListAdrees = Searching.byLocalisation(rooteList, "W");
+            List<Root> rooteListAdrees = Search.byLocalisation(rooteList);
 
             Console.WriteLine(rooteListAdrees[0].HotelInfo.Address);
-            Console.WriteLine(rooteListAdrees[1].HotelInfo.Address);
-            Console.WriteLine(rooteListAdrees[2].HotelInfo.Address);
+            //Console.WriteLine(rooteListAdrees[1].HotelInfo.Address);
+            //Console.WriteLine(rooteListAdrees[2].HotelInfo.Address);
 
 
 
