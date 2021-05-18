7f72eace2cbd18ab9c6bea7d93499091acd21b55
Łukasz Balcerak <ubalcerak@gmail.com>
2021-05-18 15:49:49 +0200

    Searching by Regexp not string

--- TravelerApp/TravelerAppCore/Models/Search.cs
+++ TravelerApp/TravelerAppCore/Models/Search.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TravelerAppCore.Models.Hotels;
 
@@ -13,10 +14,12 @@ namespace TravelerAppCore.Models
         {
             List<Root> hotelLocalisation = new List<Root>();
             string adress = getAdress();
+            string regPattern = @$">{adress}<";
+            Regex rx = new Regex(regPattern, RegexOptions.IgnoreCase);
 
             foreach(Root hotel in targetData)
             {
-                if(hotel.HotelInfo.Address == adress)
+                if(rx.Matches(hotel.HotelInfo.Address).Count != 0)
                 {
                     hotelLocalisation.Add(hotel);
                 }
--- TravelerApp/TravelerConsole/Program.cs
+++ TravelerApp/TravelerConsole/Program.cs
@@ -12,8 +12,8 @@ namespace TravelerFacebookConsole
             HotelInfo hotelOne = new HotelInfo();
             HotelInfo hotelTwo = new HotelInfo();
             HotelInfo hotelThree = new HotelInfo();
-            hotelOne.Address = "Warszawa";
-            hotelTwo.Address = "Wrocław";
+            hotelOne.Address = ">Warszawa<";
+            hotelTwo.Address = "<address class=\"addressReset\"><span rel=\"v:address\"><span dir=\"ltr\"><span class=\"street-address\" property=\"v:street-address\">1025 de Bleury Street</span>, <span class=\"locality\"><span property=\"v:locality\">Warszawa</span>, <span property=\"v:region\">Quebec</span> <span property=\"v:postal-code\">H2Z 1M7</span></span>, <span class=\"country-name\" property=\"v:country-name\">Canada</span> </span></span></address>";
             hotelThree.Address = "Wołomin";
             Root rootOne = new Root();
             Root rootTwo = new Root();
@@ -30,7 +30,7 @@ namespace TravelerFacebookConsole
             List<Root> rooteListAdrees = Search.byLocalisation(rooteList);
 
             Console.WriteLine(rooteListAdrees[0].HotelInfo.Address);
-            //Console.WriteLine(rooteListAdrees[1].HotelInfo.Address);
+            Console.WriteLine(rooteListAdrees[1].HotelInfo.Address);
             //Console.WriteLine(rooteListAdrees[2].HotelInfo.Address);
 
 
