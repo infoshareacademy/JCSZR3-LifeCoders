1464e4e76851b1dbb706b4bfafb3adfcc5d69868
Łukasz Balcerak <ubalcerak@gmail.com>
2021-05-18 14:06:28 +0200

    Added class Search with first logic,and some test in Program

---
+++ TravelerApp/TravelerAppCore/Models/Search.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelerAppCore.Models.Hotels;
+
+namespace TravelerAppCore.Models
+{
+    public static class Searching
+    { 
+        public static List<Root> byLocalisation(List<Root> targetData, string localisation)
+        {
+            List<Root> hotelLocalisation = new List<Root>();
+
+            foreach(Root hotel in targetData)
+            {
+                if(hotel.HotelInfo.Address == localisation)
+                {
+                    hotelLocalisation.Add(hotel);
+                }
+            }
+
+            return hotelLocalisation;
+        }
+    }
+}
--- TravelerApp/TravelerConsole/Program.cs
+++ TravelerApp/TravelerConsole/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using TravelerAppCore.Models.Hotels;
+using TravelerAppCore.Models;
 
 namespace TravelerFacebookConsole
 {
@@ -6,7 +9,32 @@ namespace TravelerFacebookConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HotelInfo hotelOne = new HotelInfo();
+            HotelInfo hotelTwo = new HotelInfo();
+            HotelInfo hotelThree = new HotelInfo();
+            hotelOne.Address = "Warszawa";
+            hotelTwo.Address = "Wrocław";
+            hotelThree.Address = "Wołomin";
+            Root rootOne = new Root();
+            Root rootTwo = new Root();
+            Root rootThree = new Root();
+            rootOne.HotelInfo = hotelOne;
+            rootTwo.HotelInfo = hotelTwo;
+            rootThree.HotelInfo = hotelThree;
+
+            List<Root> rooteList = new List<Root>();
+            rooteList.Add(rootOne);
+            rooteList.Add(rootTwo);
+            rooteList.Add(rootThree);
+
+            List<Root> rooteListAdrees = Searching.byLocalisation(rooteList, "W");
+
+            Console.WriteLine(rooteListAdrees[0].HotelInfo.Address);
+            Console.WriteLine(rooteListAdrees[1].HotelInfo.Address);
+            Console.WriteLine(rooteListAdrees[2].HotelInfo.Address);
+
+
+
         }
     }
 }
