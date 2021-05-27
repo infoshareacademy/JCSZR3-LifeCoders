6736075a85fa77214137156013e4ea531d5f2eb2
Joanna Kozłowska <joakoz92@wp.pl>
2021-05-27 16:38:21 +0200

    class sort added

---
+++ TravelerApp/TravelerAppCore/Controller/Sort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelerAppCore.Models.Hotels;
+
+namespace TravelerAppCore.Controller
+{
+   public class Sort
+    {
+        public static void sort(List<Root> targetData)
+        {
+            List<Root> hotelPrice = new List<Root>();
+            foreach (Root hotel in targetData)
+            {
+                if (hotel.HotelInfo.Price != null)
+                {
+                    hotelPrice.Add(hotel);
+                }
+                
+               
+            }
+
+           // hotelPrice.Sort();
+
+            foreach (Root hotel1 in hotelPrice)
+            {
+                Console.WriteLine(hotel1.HotelInfo.Name+" "+hotel1.HotelInfo.Price);
+
+
+            }
+        }
+
+
+
+    }
+}
