991a222e12e8f3d055781f2858c83f05c9eacd35
Joanna Kozłowska <joakoz92@wp.pl>
2021-05-28 14:54:29 +0200

    method sort works

--- TravelerApp/TravelerAppCore/Controller/Sort.cs
+++ TravelerApp/TravelerAppCore/Controller/Sort.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TravelerAppCore.Models.Hotels;
+using TravelerAppCore.View;
 
 namespace TravelerAppCore.Controller
 {
@@ -11,28 +12,35 @@ namespace TravelerAppCore.Controller
     {
         public static void sort(List<Root> targetData)
         {
-            List<Root> hotelPrice = new List<Root>();
-            foreach (Root hotel in targetData)
+            List<Root> hotelRatings = targetData;
+
+
+            // hotelPrice.Sort();
+
+            hotelRatings.Sort(IComparer);
+
+
+            DrawTable.PrintLine();
+            DrawTable.PrintRow(true,"hotelId", "Name", "Price", "Ratings");
+
+            foreach (var hotel in hotelRatings)
             {
-                if (hotel.HotelInfo.Price != null)
-                {
-                    hotelPrice.Add(hotel);
-                }
+                DrawTable.PrintLine();
+                DrawTable.PrintRow(true, hotel.HotelInfo.HotelID, hotel.HotelInfo.Name, hotel.HotelInfo.Price, hotel.Reviews[2].Ratings.Overall.ToString());
                 
-               
             }
-
-           // hotelPrice.Sort();
-
-            foreach (Root hotel1 in hotelPrice)
-            {
-                Console.WriteLine(hotel1.HotelInfo.Name+" "+hotel1.HotelInfo.Price);
-
-
-            }
+            DrawTable.PrintLine();
+            
         }
 
-
-
+        private static int IComparer(Root x, Root y)
+        {
+            if (x.Reviews[2].Ratings.Overall < y.Reviews[2].Ratings.Overall)
+                return 1;
+            else if (x.Reviews[2].Ratings.Overall > y.Reviews[2].Ratings.Overall)
+                return -1;
+            else
+                return 0;
+        }
     }
 }
