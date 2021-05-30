7fb6c1ca1ed53faed6c0f2f3e7daae2325315a43
Łukasz Balcerak <ubalcerak@gmail.com>
2021-05-30 13:46:01 +0200

    Clean up code

--- TravelerApp/TravelerAppCore/Controller/AddressFormatFixer.cs
+++ TravelerApp/TravelerAppCore/Controller/AddressFormatFixer.cs
@@ -15,38 +15,12 @@ namespace TravelerAppCore.Controller
             foreach (Root hotel in baseData)
             {
                 var address = Regex.Replace(hotel.HotelInfo.Address, "<.*?>", string.Empty);
-                int commaCounter = 0;
-
+                
                 Regex rg = new Regex(@"(?<street>.+),\s(?<city>.+),\s(?<postalCode>.+)");
                 var addressMatch = rg.Match(address);
                 string street = addressMatch.Groups["street"].Value;
                 string city = addressMatch.Groups["city"].Value;
                 string postalCode = addressMatch.Groups["postalCode"].Value;
-
-                //foreach (var letter in hotel.HotelInfo.Address)
-                //{
-                //    if (letter != ',')
-                //    {
-                //        switch (commaCounter)
-                //        {
-                //            case 0:
-                //                street += letter;
-                //                break;
-                //            case 1:
-                //                city += letter;
-                //                break;
-                //            case 2:
-                //                postalCode += letter;
-                //                break;
-                //            default:
-                //                break;
-
-                //        }
-                //    } else
-                //    {
-                //        commaCounter++;
-                //    }
-                // }
 
                 street = street.Trim();
                 city = city.Trim();
