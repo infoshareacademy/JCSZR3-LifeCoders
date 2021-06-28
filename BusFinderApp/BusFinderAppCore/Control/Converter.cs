90f348b2183fdf76857103615547dd202c88122d
Łukasz Balcerak <ubalcerak@gmail.com>
2021-06-28 11:38:23 +0200

    TimeStampConverter Added
    Added Class with method to convert timestamp into DateTime and save it.

---
+++ BusFinderApp/BusFinderAppCore/Control/Converter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BusFinderAppCore.Models;
+
+namespace BusFinderAppCore.Control
+{
+    public class Converter
+    {
+        public static void TimestampToDate()
+        {
+            DateTime beginDate = new DateTime(1970, 1, 1, 0, 0, 0);
+            foreach (ScheduleForStation schedule in JSON.ShceduleList)
+            {
+                foreach (Itinerary arrival in schedule.schedule.arrivals)
+                {
+                    double ArivalTimestamp = (double)arrival.datetime.timestamp;
+                    arrival.datetime.Date = beginDate.AddSeconds(ArivalTimestamp);
+                }
+
+                foreach (Itinerary departure in schedule.schedule.departures)
+                {
+                    double DeparturesTimestamp = (double) departure.datetime.timestamp;
+                    departure.datetime.Date = beginDate.AddSeconds(DeparturesTimestamp);
+                }
+            }
+
+        }
+    }
+}
--- BusFinderApp/BusFinderAppCore/Models/Datetime.cs
+++ BusFinderApp/BusFinderAppCore/Models/Datetime.cs
@@ -1,8 +1,11 @@
-﻿namespace BusFinderAppCore.Models
+﻿using System;
+
+namespace BusFinderAppCore.Models
 {
     public class Datetime
     {
         public int timestamp { get; set; }
         public string tz { get; set; }
+        public DateTime Date { get; set; }
     }
 }
