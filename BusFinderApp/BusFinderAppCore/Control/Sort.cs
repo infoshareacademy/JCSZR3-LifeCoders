6df29cc342d41faa1df9446da92b17a1faf8391d
Joanna Kozłowska <joakoz92@wp.pl>
2021-07-10 16:10:08 +0200

    refactoring codecode cleanup

--- BusFinderApp/BusFinderAppCore/Control/Sort.cs
+++ BusFinderApp/BusFinderAppCore/Control/Sort.cs
@@ -7,46 +7,32 @@ using BusFinderAppCore.Models;
 
 namespace BusFinderAppCore.Control
 {
-   public class Sort
+    public class Sort
     {
 
-        public static List<ScheduleForStation> SortbyTown(string option)
+        public static List<ScheduleForStation> SortByTown(string option)
         {
-            List<ScheduleForStation> ShceduleList = new List<ScheduleForStation>();
-            JSON.ShceduleList = JSON.LoadJsonFiles<ScheduleForStation>("Data");
-
             if (option == "A")
                 return JSON.ShceduleList.OrderBy(x => x.station.default_address.Town).ToList();
             else if (option == "Z")
                 return JSON.ShceduleList.OrderByDescending(x => x.station.default_address.Town).ToList();
             else return
                     JSON.ShceduleList;
-        
-          
         }
-        public static List<ScheduleForStation> SortbyStreet(string option)
+        public static List<ScheduleForStation> SortByStreet(string option)
         {
-            List<ScheduleForStation> ShceduleList = new List<ScheduleForStation>();
-            JSON.ShceduleList = JSON.LoadJsonFiles<ScheduleForStation>("Data");
-
             if (option == "A")
                 return JSON.ShceduleList.OrderBy(x => x.station.default_address.Street).ToList();
             else if (option == "Z")
                 return JSON.ShceduleList.OrderByDescending(x => x.station.default_address.Street).ToList();
             else return
                     JSON.ShceduleList;
-
-
         }
         public static List<Itinerary> SortByDate()
         {
-
-            List<ScheduleForStation> ShceduleList = new List<ScheduleForStation>();
-            JSON.ShceduleList = JSON.LoadJsonFiles<ScheduleForStation>("Data");
-            var itinerary= JSON.ShceduleList.SelectMany(x=>x.schedule.departures).ToList();
+            var itinerary = JSON.ShceduleList.SelectMany(x => x.schedule.departures).ToList();
             return itinerary.OrderBy(x => x.datetime.timestamp).ToList();
-
         }
-  
+
     }
 }
