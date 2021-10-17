c1397838d888bef886897b02e6a3e4b02027c580
GrzegorzKasprzyk <xankrigor87@gmail.com>
2021-10-17 13:00:39 +0200

    changes

---
+++ BusFinderApp/RaportAktywnosciUzytkownikow/Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RaportAktywnosciUzytkownikow.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RaportAktywnosciUzytkownikow.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        static List<User> users = new List<User>();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public IActionResult Past([FromBody] User user)
+        {
+            users.Add(user);
+            return Ok(user);
+        }
+    }
+}
